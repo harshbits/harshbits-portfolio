6d779bcaa275b446a6be5b78a628b66a6b1b3586
harshbhavsar <harsh.bhavsar@copart.com>
2017-07-31 23:58:54 -0500

    drawer redesigned

--- src/components/LeftDrawer/leftDrawer.jsx
+++ src/components/LeftDrawer/leftDrawer.jsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import './leftDrawer.css';
 import { Link } from 'react-router'
+import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import FontIcon from 'material-ui/FontIcon';
 
 class LeftDrawer extends Component {
 
@@ -16,8 +20,15 @@ class LeftDrawer extends Component {
           open={this.props.open}
           onRequestChange={(open) => this.props.handleClose()}
           >
-          <MenuItem><a href="">Menu Item</a></MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
+          <AppBar
+            title="Harsh Bhavsar"
+            showMenuIconButton={false}
+            style={{ backgroundColor: '#217DBB', height: '72px' }} />
+          <Menu>
+          <MenuItem leftIcon={<FontIcon className='fa fa-chef-hat' />}>Cognitive Cooking</MenuItem>
+          <MenuItem>Advanced Farming System</MenuItem>
+          <MenuItem>Home Coumputing System</MenuItem>
+          </Menu>
         </Drawer>
       </div>
     );
