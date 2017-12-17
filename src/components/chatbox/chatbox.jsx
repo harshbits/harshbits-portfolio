f29ab7013d15962b4598f61d0f56ddaf0de0cdd9
Harsh Bhavsar <hbhavsar2110@yahoo.com>
2017-12-17 10:24:25 -0600

    [REL_1.0.1] Chatbox with welcome message

--- src/components/chatbox/chatbox.jsx
+++ src/components/chatbox/chatbox.jsx
@@ -8,25 +8,31 @@ import React, {Component} from 'react';
 import {render} from 'react-dom'
 import './chatbox.css';
 import {Launcher} from 'react-chat-window';
+import messageHistory from '../../messageHistory';
 
 class ChatBox extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-       messageList: props.messageHistory || []
+       messageList: messageHistory,
+      newMessagesCount: 0
     };
   }
 
   _onMessageWasSent(message) {
+    console.log('on message sent');
     this.setState({
       messageList: [...this.state.messageList, message]
     })
   }
 
   _sendMessage(text) {
+    console.log('send message');
     if (text.length > 0) {
+      const newMessagesCount = this.state.isOpen ? this.state.newMessagesCount : this.state.newMessagesCount + 1
       this.setState({
+        newMessagesCount: newMessagesCount,
         messageList: [...this.state.messageList, {
           author: 'them',
           type: 'text',
@@ -46,6 +52,7 @@ class ChatBox extends Component {
         }}
         onMessageWasSent={this._onMessageWasSent.bind(this)}
         messageList={this.state.messageList}
+        newMessagesCount={this.state.newMessagesCount}
       />
     </div>);
   }
---
+++ src/messageHistory.js
@@ -0,0 +1,3 @@
+export default [
+  {type: 'text', author: "them", data: { text: "Hi, I'm Eunice. How can I help you? 😊"} }
+]
